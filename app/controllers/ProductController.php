bf336725c2a6254ec263c290b3336a86ab05475f
Sergey Z <sergey.z@thewhitelabelagency.com>
2022-07-21 15:19:25 +0300

    Add Single Product page

--- app/controllers/ProductController.php
+++ app/controllers/ProductController.php
@@ -2,9 +2,28 @@
 
 namespace app\controllers;
 
+use app\models\Product;
+use zpdevfrw\App;
+
+/**
+ * @property Product $model
+ *
+ * */
 class ProductController extends AppController
 {
-    public function viewAction() {
-
+    public function viewAction()
+    {
+        $lang = App::$app->getProperty('language');
+        
+        $product = $this->model->get_product($this->route['slug'], $lang);
+        
+        if ( ! $product) {
+            throw new \Exception("Product {$this->route['slug']} not found", 404);
+        }
+        
+        $gallery = $this->model->get_gallery($product['id']);
+        
+        $this->setMeta($product['title'], $product['description'], $product['keywords'],);
+        $this->set(compact('product', 'gallery'));
     }
 }
