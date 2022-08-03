0d1f8228b59806d8649837156c630a08f6df54c5
Sergey Z <sergey.z@thewhitelabelagency.com>
2022-08-03 16:49:28 +0300

    Add admin dashboard widgets

--- app/controllers/admin/MainController.php
+++ app/controllers/admin/MainController.php
@@ -2,13 +2,19 @@
 
 namespace app\controllers\admin;
 
+use RedBeanPHP\R;
+
 class MainController extends AppController
 {
     public function indexAction()
     {
+        $orders = R::count('orders');
+        $new_orders = R::count('orders', 'status = 0');
+        $users = R::count('user');
+        $products = R::count('product');
         $title = 'Dashboard';
 //        TODO admin page meta doesn't work
         $this->setMeta("Back-office :: {$title}}");
-        $this->set(compact('title'));
+        $this->set(compact('title', 'orders', 'new_orders', 'users', 'products'));
     } 
 }
