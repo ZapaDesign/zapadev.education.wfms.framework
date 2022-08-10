c2eb3275d302ce482c37aa75555f0376d89c501d
zapaDEV <zapa.poltava@gmail.com>
2022-08-11 00:16:26 +0300

    start heroku

---
+++ config/config_db.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'dsn'      => 'mysql:host=148.251.75.177:3310;dbname=zapar341_e7n_wfms_framework;charset=utf8',
+    'user'     => 'zapar341_zapadev',
+    'password' => 'gU3fS4hA2ubA2oD7cY5q'
+];
